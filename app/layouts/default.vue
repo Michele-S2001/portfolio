30ffad1d90355e4e55afab76a62487b04f26cad3
Michele-S2001 <serafini.michele01@gmail.com>
2024-09-30 17:08:53 +0200

    Added - to header a link with my name that brings to the main page & some comments

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,8 +1,19 @@
 <template>
-  <div>
-    <header>
+  <div class="container">
+    <!-- inizio header 
+      IDEE: possibilità di creare un componente apposta per l'header
+    -->
+    <header class="flex justify-between items-center my-5">
+      <div>
+        <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200">Michele Serafini</NuxtLink>
+      </div>
       <Navbar />
     </header>
-    <slot></slot>
+    <!-- fine header -->
+    <!-- inizio main content -->
+    <main class="container">
+      <slot></slot>
+    </main>
+    <!-- fine main content -->
   </div>
 </template>
