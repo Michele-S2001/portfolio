f9b46c7996b63169cd59424faf7bcd5059e83d0c
Michele-S2001 <serafini.michele01@gmail.com>
2024-09-30 17:06:55 +0200

    Added - override container class default settings

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: '0.5rem',
+        screens: {
+          '2xl': '42rem'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
