2a934ce6e5e6ab297112fc2ae355145886d6d43c
Michele-S2001 <serafini.michele01@gmail.com>
2024-09-30 15:55:47 +0200

    Added - raw blog views

---
+++ app/pages/blog/[id].vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>
+    article detail
+  </h1>
+  <div>Post id is {{ route.params.id }}</div>
+</template>
+
+<script setup>
+const route = useRoute();
+
+console.log(route);
+
+</script>
---
+++ app/pages/blog/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>All blog posts list</h1>
+  </div>
+</template>
