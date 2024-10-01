010d8aa144ed7568ba829f546f5f2df47d7da41f
Michele-S2001 <serafini.michele01@gmail.com>
2024-10-01 10:21:05 +0200

    Added - raw api call inside project-list component

--- app/components/Project-list.vue
+++ app/components/Project-list.vue
@@ -1,3 +1,14 @@
 <template>
-  <div></div>
+  <div>list</div>
+  <section v-if="error">Something went wrong, try again</section>
+  <section v-else-if="!(status === 'success')">Loading...</section>
+  <section v-else>
+    Here we display the repos!
+  </section>
 </template>
+
+<script setup>
+const { data, status, error } = await useFetch('https://api.github.com/users/michele-s2001/repos');
+console.log(data.value);
+
+</script>
