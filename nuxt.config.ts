f3d7f7b24f31d99e0283d802aa24d7a0ebdd2f8a
Michele-S2001 <serafini.michele01@gmail.com>
2024-10-01 15:07:44 +0200

    Added - initial setup for using nuxt content module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,8 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss',
-    '@nuxtjs/color-mode'
+    '@nuxtjs/color-mode',
+    '@nuxt/content'
   ],
   colorMode: {
     classSuffix: ''
