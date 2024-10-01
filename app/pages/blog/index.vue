82293300a8dd05c4b27d245270a03040ec96590c
Michele-S2001 <serafini.michele01@gmail.com>
2024-10-01 17:45:57 +0200

    Added - fetching all blog posts with queryContent composable & displayed on the page

--- app/pages/blog/index.vue
+++ app/pages/blog/index.vue
@@ -1,11 +1,20 @@
 <template>
-  <div>
-    <h1>All blog posts list</h1>
-  </div>
+  <section>
+    <h1 class="text-2xl font-bold mb-7">All blog posts list</h1>
+    <ul>
+      <li v-for="post in posts" :key="post._path">
+        <NuxtLink :to="post._path">{{ post.title }}</NuxtLink>
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script setup>
 useSeoMeta({
   title: 'blog'
 })
+
+const { data:posts } = await useAsyncData(
+  'blog-list', () => queryContent('/blog').only(['_path', 'title']).find()
+);
 </script>
